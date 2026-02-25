bf8c4ee04853d8ed23988bc7729ceb5e5cabd871
Forzun <vbhavesh219@gmail.com>
2026-02-25 18:40:55 +0530

    addingxAckBulk

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,9 +1,11 @@
+import "dotenv/config";
 import {createClient } from "redis"
 import axios from "axios";
 import { prisma } from "@workspace/database"
 
 const CONSUMER_GROUP_NAME='us';
 const STREAM_KEY='nexstack:website';
+const REGION_ID = '1'
 
 type message = { 
     id: string; 
@@ -34,24 +36,30 @@ try{
     
     const response = await xReadGroup(CONSUMER_GROUP_NAME , '1');
 
-    const promises = response.map(({id , message}) => { 
-        new Promise<void>((resolve , reject ) => {
+    const promises = response.map(({message}) => { 
+        return new Promise<void>((resolve , reject ) => {
         const startTime = Date.now();
         const websiteId = message.id;
         const websiteUrl = message.url;
-        axios.get(websiteUrl)
+        console.log({
+            websiteId, 
+            websiteUrl
+        })
+        axios.get('https://google.com')
             .then(async() =>  {
+                console.log("atlest once")
                 const endTime = Date.now() 
                 await prisma.websiteTick.create({
                     data: { 
-                        response_time: endTime- startTime,
+                        response_time: endTime - startTime,
                         status: "UP", 
                         website_id: websiteId,
-                        createdAt: new Date(),
                     }
                 })
                 resolve();
-            }).catch(async() => {
+            }).catch(async(error) => {
+                console.log(error.error)
+                console.log(error.message)
                 const endTime = Date.now()
                 await prisma.websiteTick.create({ 
                     data: {
@@ -64,10 +72,11 @@ try{
                 resolve()
             })
     })
-})
+    })
     await Promise.all(promises)    
+    console.log("promise length", promises.length)
 
-
+    xAckBulk(CONSUMER_GROUP_NAME , response.map(({id}) => id))
 }catch(error){
     console.log(error)
 }
@@ -89,11 +98,17 @@ async function xReadGroup(consumerGroup:string , workerId:string):Promise<messag
     
     // @ts-ignore
     let messages:message[] = res[0].messages;
+    console.log("read message", messages)
     return messages
 }
 
-async function xAck(steamKey: string , consumerId: string , id: string){ 
+async function xAck(consumerGroup: string , id: string){ 
 
-    const response = await client.xAck(steamKey , consumerId , id);
+    const response = await client.xAck(STREAM_KEY , consumerGroup , id);
     return response;
 }
+
+async function xAckBulk(consumerGroup: string , eventIds: string[]){ 
+    eventIds.map(eventId => xAck(consumerGroup , eventId))
+}
+
